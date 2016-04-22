b7b56dfb144fd399a4a5b7f3da0f35533afb0b04
Keith Choison <mickeychoison@sbcglobal.net>
2016-04-21 19:44:37 -0700

    Initial Commit

---
+++ lab1/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  lab1
+//
+//  Created by Keith Choison on 4/21/16.
+//  Copyright © 2016 kchoison. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
